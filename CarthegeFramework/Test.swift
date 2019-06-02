2198780d516142e922d27105f6396a455ef93178
ape0108792 <ape0108792@naver.com>
2019-06-02 22:58:11 +0900

    fff

---
+++ CarthegeFramework/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  CarthegeFramework
+//
+//  Created by 황도증 on 02/06/2019.
+//  Copyright © 2019 황도증. All rights reserved.
+//
+import Foundation
+
+public class CarthegeTest: NSObject {
+    public static let descriptions = "Helle, Carthege"
+    
+    public func sayWord() {
+        print(CarthegeTest.descriptions)
+    }
+}
